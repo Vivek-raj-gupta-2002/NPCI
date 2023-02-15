450de136d50e673d06e0903d823436eec9e85141
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2023-02-16 01:50:34 +0530

    Completed

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const path = require('path');
+const Search = require('./true');
+const bodyParser = require('body-parser');
 
-const Search = require('./true')
+const session = require('express-session');
 
 const app = express();
 
+app.use(session({
+    secret: 'Your_Secret_Key',
+    resave: true,
+    saveUninitialized: true
+}))
+
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "statics")));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,35 +27,38 @@ app.use((req, res, next) => {
   });
 
 
-app.post('/api', (req, res, next) => {
-
-    
-
-    if(req.body.ph !== undefined){
-
-        let data = Search(req.body.ph);
-
-        data.then(function(responce){
-
-        responce = JSON.parse(responce)
-
-        res.status(201).json({
-            responce
-        });
-
-    })
-
-    }else{
-        res.status(400).json({
-            message: "Bad Request"
-        });
-    }
-
-    
-
-    
-
+app.get('/', (req, res, next)=>{
+    res.sendFile('/front-end/index.html', { root: __dirname });
 })
 
+
+app.get('/pin', (req, res, next) => {
+    res.sendFile('front-end/file.html', { root: __dirname })
+})
+
+app.post('/check', (req, res, next)=>{
+    let ph = req.body.ph;
+    let amount = req.body.amount;
+    
+    
+    let data = Search(ph);
+
+    req.session.ph = ph;
+    req.session.amount = amount;
+
+    res.redirect('/pin');
+    
+})
+
+app.get('/getData', (req, res, next) => {
+    res.send({
+        amount: req.session.amount,
+        ph: req.session.ph,
+    })
+})
+
+app.get('/done', (req, res, next) => {
+    res.sendFile('front-end/completed.html', { root: __dirname })
+})
 
 module.exports = app;
